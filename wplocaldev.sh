3f84cc50defdd8b3fe104fb9dff7c21b4c6ddf00
azhsetiawan <azh@sribu.com>
2016-05-25 18:30:55 +0700

    Mark 1 Sukses

--- wplocaldev.sh
+++ wplocaldev.sh
@@ -10,65 +10,98 @@
 # install WordPress
 # open site in browser
 
-# CHECK_APACHE=""
-# CHECK_MYSQL=""
-# CHECK_WP_CLI=""
+# Check the number of arguments
+if [ $# -lt 1 ]; then
+  echo
+  echo "Please type arguments"
+  echo "Usage: $0 project (optional)dbpass (optional)arg3"
+  echo
+  exit
+fi
+
+CHECK_OS="$(uname -s)"
+
+PROJECT_NAME="$1"
+DBNAME="$1_db"
+DBPASS=${2:-root}
+
+# echo "$DBNAME"
+# echo "$DBPASS"
+
+read -p "Are you using http://localhost/$1 as url? [y/n] " URL1
+
+if [ "$URL1" == "y" ]; then
+  PROJECT_URL="http://localhost/$1"
+else
+  read -p "Are you using http://$1.dev as url? [y/n] " URL2
+
+  if [ "$URL2" == "y" ]; then
+    PROJECT_URL="http://$1.dev"
+  fi
+fi
+
+# echo "$PROJECT_URL"
+
+
+if [ ! -z "$(which apache2ctl)" ]; then
+  DOCROOT="$( apache2ctl -S | grep 'Main DocumentRoot: ' | tr -d '"' | awk ' { print $3 } ' )"
+elif [ ! -z "$(which apachectl)" ]; then
+  DOCROOT="$( apachectl -S | grep 'Main DocumentRoot: ' | tr -d '"' | awk ' { print $3 } ' )"
+elif [ ! -z "$(which httpd)" ]; then
+  DOCROOT="$( httpd -S | grep 'Main DocumentRoot: ' | tr -d '"' | awk ' { print $3 } ' )"
+fi
+
+PROJECT_DIR="$DOCROOT/$1"
+
+# echo "$PROJECT_DIR"
+
+# MY_PARAM=${1:-default}
 
 WP_CLI_DIR="$HOME/.wp-cli"
 
-DBNAME="$1"
-
-echo "$1"
-echo "$DBNAME"
-
-# PROJECT_URL=""
-# CONST=""
-# ADMIN_USER=""
-# ADMIN_PASS=""
-# ADMIN_MAIL="$ADMIN_USER@email.com"
-
-# Check MySQL
-if [ ! -z "$(which mysql)" ]; then
-  echo 'mysql installed'
-
-  if [ "$(pgrep mysql | wc -l)" -ne 1 ]; then
-    echo 'mysql down'
-    exit 1
-  else
-    echo 'mysql up!'
-  fi
-else
-  echo 'mysql not installed'
-fi
-
+# echo "$CHECK_OS"
 
 # Check WP-CLI
 if [ ! -z "$(which wp)" ]; then
-  echo 'wp-cli installed'
+  echo 'WP-CLI installed'
 
-  mkdir -p "$WP_CLI_DIR"
+  # mkdir -p "$WP_CLI_DIR"
+
+  CHECK_WP_CLI="true"
 else
-  echo 'wp-cli not installed'
+  echo 'WP-CLI is not installed'
+  echo 'This script requires WP-CLI for running'
+  exit 1
 fi
 
-# Project URL
-if [ ! -z "$(which dnsmasq)" ]; then
-  echo 'dnsmasq'
-  PROJECT_URL="http://$1.dev"
-else
-  PROJECT_URL="http://localhost/$1"
+# echo "$CHECK_WP_CLI"
+
+if [ "$CHECK_WP_CLI" == "true" ]; then
+
+  mkdir -p "$PROJECT_DIR"
+
+  cd "$PROJECT_DIR"
+
+  wp core download
+
+  wp core config --dbhost=localhost --dbname="$DBNAME" --dbprefix=wpld_ --dbuser=root --dbpass="$DBPASS" 
+
+  wp db create 
+
+  wp core install --url="$PROJECT_URL" --title="Test WPLOCALDEV" --admin_user=admin --admin_password="admin" --admin_email=admin@email.com 
+
+  # Delete plugin: hello dolly
+  wp plugin delete hello
+
 fi
 
 
-wp core download
 
-wp core config --dbhost=localhost --dbname="$DBNAME" --dbprefix=wp16_ --dbuser=root --dbpass="root" 
 
-wp db create 
-
-wp core install --url="$PROJECT_URL" --title="SiteTitle" --admin_user=admin --admin_password="admin" --admin_email=azh@sribu.com 
-
-# if [ "$(uname -s)" != "Darwin" ]; then
-#   echo "Sorry, Pow requires Mac OS X to run." >&2
-#   exit 1
-# fi
+if [ "$CHECK_OS" == "Linux" ]; then
+  # echo 'Linux'
+  xdg-open "$PROJECT_URL"
+elif [ "$CHECK_OS" == "Darwin" ]; then
+  # echo 'Darwin'
+  open "$PROJECT_URL"
+fi
